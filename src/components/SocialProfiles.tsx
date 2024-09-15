b796bc37a0c6c2c9c19130936a8632d3d26fd3ad
Adarsh chaurasia <chaurasiadarsh11@gmail.com>
2024-09-15 13:58:15 +0530

    Leetcode Status Display Feature

--- src/components/SocialProfiles.tsx
+++ src/components/SocialProfiles.tsx
@@ -4,7 +4,12 @@ import { HoverEffect } from "./ui/card-hover-effect";
 
 import * as Logo from '/portfolio/src/app/images/Pic.png';
 
-import Link from "next/link"
+import Link from "next/link";
+import Image from "next/image";
+import { useState, useEffect } from 'react';
+
+
+
 export const projects = [
     {
       title: "GitHub",
@@ -14,23 +19,44 @@ export const projects = [
       
     },
     {
+      title: "LeetCode",
+      description:
+        "",
+      link: "https://leetcode.com/u/adarshChaurasia/",
+      
+    },
+    {
         title: "LinkedIn",
         description:
           "",
         link: "https://www.linkedin.com/in/adarshchaurasia5",
         
-      },
-      {
-        title: "LeetCode",
-        description:
-          "",
-        link: "https://leetcode.com/u/adarshChaurasia/",
-        
       }
+      
       
      
 ]
+
+
+
+
 function SocialProfiles() {
+
+
+  const [imageUrl, setImageUrl] = useState('');
+
+
+
+  useEffect(() => {
+    const fetchImage = async () => {
+      const res = await fetch('https://leetcard.jacoblin.cool/adarshChaurasia');
+      const imageBlob = await res.blob();
+      setImageUrl(URL.createObjectURL(imageBlob));
+    };
+
+    fetchImage();
+  }, []);
+
   return (
 
     <div className='p-12 bg-gray-900 '>
@@ -40,10 +66,18 @@ function SocialProfiles() {
         <div className="text-center">
         <p className='mt-2 text-3xl leading-8 font-extrabold tracking-tight text-white sm:text-4xl'>Social Profiles</p>
         </div>
+        
         <div className="mt-10 text-center">
           
             <HoverEffect items={projects}/>
+        </div>
 
+
+        <div className="flex flex-column justify-center items-center">
+
+        {imageUrl ? <img src={imageUrl} 
+            className="rounded-lg shadow-xl shadow-gray-800 sm:max-w-xs md:max-w-sm items-center"
+     alt="Fetched Image" /> : <p>Loading...</p>}
 
 
 
