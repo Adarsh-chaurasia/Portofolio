7b6bdd7a63954974790bfaf81d9b06580d9100ea
Adarsh chaurasia <chaurasiadarsh11@gmail.com>
2024-09-29 12:44:08 +0530

    Resume Link

--- src/app/contact/page.jsx
+++ src/app/contact/page.jsx
@@ -123,7 +123,7 @@ function Contact() {
 
 
 
-      { show && <Link href = {'https://drive.google.com/file/d/10W8Fdcp8UTwcfkVb6jSpWTmuPX6wDSXx/view?usp=drive_link'}
+      { show && <Link href = {'https://drive.google.com/file/d/1hj4-6b3iC-WYU1g2iGAZSJoD_EYkfMbU/view'}
               target="_blank"> 
               
               <button  className="px-8 py-2 mt-2 rounded-lg bg-teal-500 text-white font-medium hover:bg-teal-600 focus:outline-none focus:ring-2 focus:ring-teal-500 focus:ring-offset-2"> Resume / CV </button>
